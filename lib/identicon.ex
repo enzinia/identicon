ae654285cd4ffeadb62915433e029c24b1750142
enzinia <vkukovskij@gmail.com>
2020-02-20 23:11:48 +0900

    Add image generating and output

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,4 +1,6 @@
 defmodule Identicon do
+  @square_size 50
+
   def main(input) do
     input
     |> hash_input
@@ -6,6 +8,8 @@ defmodule Identicon do
     |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
   end
 
   def hash_input(input) do
@@ -44,14 +48,30 @@ defmodule Identicon do
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
     pixel_map = Enum.map grid, fn({_code, index}) ->
-      horizontal = rem(index, 5) * 50
-      vertical = div(index, 5) * 50
+      horizontal = rem(index, 5) * @square_size
+      vertical = div(index, 5) * @square_size
       top_left = {horizontal, vertical}
-      bottom_right = {horizontal + 50, vertical + 50}
+      bottom_right = {horizontal + @square_size, vertical + @square_size}
 
       {top_left, bottom_right}
     end
 
     %Identicon.Image{image | pixel_map: pixel_map}
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image_size = @square_size * 5
+    image = :egd.create(image_size, image_size)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def save_image(image, filename) do
+    File.write("#{filename}.png", image)
   end
 end
